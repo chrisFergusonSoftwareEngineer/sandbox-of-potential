560edb8ae076c30d54a057ae090aaa11255be3d9
chrisFergusonSoftwareEngineer <chrisferguson.softwareengineer@gmail.com>
2021-05-21 10:44:10 -0400

    Overhead updates.

--- GOLANG/cmd/main.go
+++ GOLANG/cmd/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/chrisFergusonSoftwareEngineer/sandbox-of-potential/GOLANG/locking"
+	"github.com/chrisFergusonSoftwareEngineer/sandbox-of-potential/GOLANG/json"
 )
 
 func main() {
 	fmt.Println("Running the SANDBOX OF POTENTIAL!!!")
 
-	locking.TryOnceLocking()
+	json.ParseJsonWithGabs()
 
 	//using defer to (hopefully) ensure other threads complete first.
 	defer fmt.Println("Exiting the SANDBOX OF POTENTIAL!!!")
