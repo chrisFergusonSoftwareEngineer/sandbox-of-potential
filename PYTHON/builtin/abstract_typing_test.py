2d82ffebdfee188f1c9438b12b424dff1249b2d3
Chris Ferguson <cferguson@nextgenfed.com>
2024-02-13 12:19:44 -0500

    Not yet working as expected.

--- PYTHON/builtin/abstract_typing_test.py
+++ PYTHON/builtin/abstract_typing_test.py
@@ -7,7 +7,7 @@ class Abstract_Test_Class:
     def hello_name(self, name:Any):
         pass
 
-# documentation, more docs
+# documentation, more docs, continuing to tinker.
 class Concrete_Test_Class(Abstract_Test_Class):
     def hello_name(self, name:str):
         print(f"Hello, {name}")
