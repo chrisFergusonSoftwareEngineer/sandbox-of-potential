26c5aa76b3ab5306c621fb12ac6601fdc2cebd34
Chris Ferguson <cferguson@nextgenfed.com>
2024-02-13 12:18:35 -0500

    More documentation to force a change.

--- PYTHON/builtin/abstract_typing_test.py
+++ PYTHON/builtin/abstract_typing_test.py
@@ -7,7 +7,7 @@ class Abstract_Test_Class:
     def hello_name(self, name:Any):
         pass
 
-# documentation
+# documentation, more docs
 class Concrete_Test_Class(Abstract_Test_Class):
     def hello_name(self, name:str):
         print(f"Hello, {name}")
