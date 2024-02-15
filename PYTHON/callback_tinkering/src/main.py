4a3d4166790bcd34526003743750043d2d95b615
Chris Ferguson <cferguson@nextgenfed.com>
2024-02-15 14:02:28 -0500

    Docstring that will trigger change.

--- PYTHON/callback_tinkering/src/main.py
+++ PYTHON/callback_tinkering/src/main.py
@@ -1,3 +1,5 @@
+"""Tinkering with threadding in Python."""
+
 # CAUTION FOR MANUAL RUNS: Each thread is independent of the main process, it does NOT SAFELY EXIT.  Nor can they be interrupted; they can only be stopped by killing the terminal instance.
 
 # demonstrates multiple threads inteleaving with the main thread.
@@ -5,14 +7,47 @@
 import time
 from subscribe_looper import SubscribeLooper
 
+
 def main_routine():
     print("starting")
-    symbol = SubscribeLooper(['!','@','#','$','%'], printScrabbleTile)
-    number = SubscribeLooper(['1','2','3','4','5','6','7','8','9','0'], printScrabbleTile)
-    alpha = SubscribeLooper(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], printScrabbleTile)
+    symbol = SubscribeLooper(["!", "@", "#", "$", "%"], printScrabbleTile)
+    number = SubscribeLooper(
+        ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"], printScrabbleTile
+    )
+    alpha = SubscribeLooper(
+        [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+        ],
+        printScrabbleTile,
+    )
 
     symbol.start()
-    
+
     number.start()
 
     alpha.start()
@@ -28,8 +63,10 @@ def main_routine():
         print("======")
         time.sleep(2)
 
+
 def printScrabbleTile(tile):
     print(" [ %s ] " % tile)
 
-if __name__=="__main__": # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
     main_routine()
